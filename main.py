0db946db0e6fc6992a6c84103b8b0d1729bd81c0
FaisalB <116850086+FaisalBesta1@users.noreply.github.com>
2022-12-23 22:22:41 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+morse_code_dict = {
+    'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.', 'H': '....',
+    'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---', 'P': '.--.', 'Q': '--.-',
+    'R': '.-.', 'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-', 'Y': '-.--', 'Z': '--..',
+    '0': '-----', '1': '.----', '2': '..---', '3': '...--', '4': '....-', '5': '.....', '6': '-....', '7': '--...',
+    '8': '---..', '9': '----.', '.': '.-.-.-', ',': '--..--', '?': '..--..', ' ': ' '
+}
+
+string = input("What do you want to turn to morse?: ")
+
+string_list = list(string)
+
+translated_string = ""
+for letter in string_list:
+    if letter == ' ':
+        translated_string += morse_code_dict[letter.title()]
+    else:
+        translated_string += f'{morse_code_dict[letter.title()]}/'
+
+print(translated_string)
